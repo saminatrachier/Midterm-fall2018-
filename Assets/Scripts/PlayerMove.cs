98c83b61f6a324cca4c0b8708d15d4731b6a95f4
Samina Trachier <st3225@nyu.edu>
2018-09-27 11:33:23 -0400

    Player Move and Camera Edit2

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float moveSpeed = 10;
+	//mouse sensitivity:
+	public float lookSpeed = 300f;
+	Vector3 inputVector; // pass data from Update() to FixedUpdate()
+	// Use this for initialization
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//mouse look
+		float mouseX = Input.GetAxis("Mouse X")* lookSpeed * Time.deltaTime; //mouseX = horizontal mouseDelta
+		float mouseY = Input.GetAxis("Mouse Y")* lookSpeed * Time.deltaTime; //mouseY = vertical mouseDelta
+		
+		//rotate capsule left/right, but rotate camera up/down
+		transform.Rotate(0f, mouseX, 0f);
+		Camera.main.transform.localEulerAngles += new Vector3(-mouseY, 0f, 0f);
+		
+		//solution: after applying rotations, un-roll the camera
+		Camera.main.transform.localEulerAngles -= new Vector3(0,0, Camera.main.transform.localEulerAngles.z);
+		
+		//first-person player movement
+
+		float vertical = Input.GetAxis("Vertical"); //corresponds to W/S or Up/Down on a Keyboard, -1 for down +1 for up
+		float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right on Keyboard, -1 is left, +1 is right
+		
+		inputVector = transform.forward * vertical * moveSpeed; //forward/back direction
+		inputVector += transform.right * horizontal * moveSpeed; //left/right direction
+	}
+	void FixedUpdate() //all physics code should go in FixedUpdate!!!
+	{
+		//apply our forces to move the object around
+		GetComponent<Rigidbody>().velocity = inputVector; //no need for Time.deltatime, already fixed framerate
+		
+		//one problem: gravity doesn't work anymore
+
+	}
+}
