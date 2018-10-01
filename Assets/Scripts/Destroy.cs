3e2b1543e3aa154df5c9c0422cde5d0ec4fd76a7
Samina Trachier <st3225@nyu.edu>
2018-09-30 22:32:16 -0400

    2nd Spawner and Destroy Script

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+    private void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "Doggo1")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+   
+}
