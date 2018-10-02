57a7ec75c8c54df7800d77004eef4c521d882eaa
Samina Trachier <st3225@nyu.edu>
2018-10-02 10:37:55 -0400

    Move the Spawners up and Create a 'Notes' Script

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//instead of a wave clear game, maybe a hide and seek game where you try to find your dogs in a sea of bigger/similar looking dogs?
+	//pick up the dog to look at it 
+	// 'e' to confirm that is your dog
+	// find 3 doggos, if you cannot find your dog in the time given, its a game over
+	//if you pick up the wrong dog it flies away/despawns
+	//if you find all the dogs its a win
+	//side-bar with the faces of the dogs you need to find
+	//(possible timer? maybe not)
+	//"It's time to leave the dog park, lets grab the boys!"
+	//"Hover over the dog to select, click to collect"
+	//if it is the wrong doggo it vanishes into smoke
+}
