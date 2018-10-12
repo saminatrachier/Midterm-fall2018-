ce6ef188b2ac30a51057e18f007f36941fe0b265
Samina Trachier <st3225@nyu.edu>
2018-10-12 13:32:11 -0400

    Created 2nd Build, Added Player Camera Clamp and Cursor Lock

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -12,6 +12,9 @@ public class PlayerMove : MonoBehaviour
 	Vector3 inputVector; // pass data from Update() to FixedUpdate()
 	// Use this for initialization
 	
+	//store Mouse Y rotation in this variable to clamp and lock the mouse
+	private float upDownRotation;
+	
 	void Start () {
 		
 	}
@@ -20,22 +23,29 @@ public class PlayerMove : MonoBehaviour
 	void Update () {
 		
 		//mouse look
-		//if (MainCamera.transform.rotation.y > 0)
-		{
-		//	MainCamera.transform.rotation= new Vector3(0)
-		}
 		float mouseX = Input.GetAxis("Mouse X")* lookSpeed * Time.deltaTime; //mouseX = horizontal mouseDelta
 		float mouseY = Input.GetAxis("Mouse Y")* lookSpeed * Time.deltaTime; //mouseY = vertical mouseDelta
 		
 		//rotate capsule left/right, but rotate camera up/down
 		transform.Rotate(0f, mouseX, 0f);
-		Camera.main.transform.localEulerAngles += new Vector3(-mouseY, 0f, 0f);
+//		Camera.main.transform.localEulerAngles += new Vector3(-mouseY, 0f, 0f);
 		
+//		BETTER MOUSE LOOK: Add mouseInput to upDOwnRotation AND clamp upDownRotation
+		upDownRotation -= mouseY;
+		upDownRotation = Mathf.Clamp(upDownRotation, -80, 80);
+
 		//solution: after applying rotations, un-roll the camera
-		Camera.main.transform.localEulerAngles -= new Vector3(0,0, Camera.main.transform.localEulerAngles.z);
+		Camera.main.transform.localEulerAngles = new Vector3(upDownRotation,0f, 0f);
+		
+		//BETTER MOUSE LOOK: Lock and hide the mouse cursor
+		if (Input.GetMouseButtonDown(0)) //0 = left click
+		{
+			Cursor.lockState = CursorLockMode.Locked; // lock cursor in the center of the screen
+			Cursor.visible = false; //hides the cursor just to be safe
+		}
+		
 		
 		//first-person player movement
-
 		float vertical = Input.GetAxis("Vertical"); //corresponds to W/S or Up/Down on a Keyboard, -1 for down +1 for up
 		float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right on Keyboard, -1 is left, +1 is right
 		
